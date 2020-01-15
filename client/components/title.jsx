2be27e072a97fcd71f0fa5790317c6cefcf95d26
Weilin Hong <weilinhong007@gmail.com>
2020-01-15 20:05:01 +0000

    finish add to cart front end

--- client/components/title.jsx
+++ client/components/title.jsx
@@ -3,7 +3,11 @@ import React from 'react';
 function Title(props) {
   return (
     <div className="title p-3 mb-5 bg-dark text-white">
-      $Wicked Sales
+      <p className="store-name">$Wicked Sales</p>
+      <div className="shopping-cart">
+        <i className="fas fa-shopping-cart"></i>
+        <p className="cart-amount">{props.cartNum}</p>
+      </div>
     </div>
   );
 }
