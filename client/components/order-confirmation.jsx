6a097570ffdf2dcb375126bcb7cec32d4e2ff2cc
weilinh98 <weilinhong007@gmail.com>
2020-02-26 10:11:22 -0800

    initialize order confirmation page

--- client/components/order-confirmation.jsx
+++ client/components/order-confirmation.jsx
@@ -6,17 +6,23 @@ class OrderConfirmation extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-
     };
   }
 
   render() {
+    const { orderId, shippingAddress, firstName, lastName, emailAddress, phoneNumber, city, state, zipCode, country } = this.context.confirmationDetail;
     return (
       <React.Fragment>
         <Title/>
-        <p>
-          {this.context.confirmationDetail}
-        </p>
+        <div className="confirmation-container row">
+          <div className="confirmation-title w-100 text-center">
+          Thank You For Your Order! {firstName} !
+          </div>
+          <div className="shipping-info-container">
+            <div className="shipping-info-title">Shipping Information:</div>
+            <p>Name: {`${firstName} ${lastName}`}</p>
+          </div>
+        </div>
       </React.Fragment>
     );
   }
